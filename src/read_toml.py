53932258aabd29d8ff4fb7741864b5401625c200
Zayne Marsh <66521670+porplax@users.noreply.github.com>
2022-10-16 21:45:56 -0400

    Delete src directory

--- src/read_toml.py
+++
@@ -1,8 +0,0 @@
-# MODULES ===========================================================
-import toml
-# FUNCTIONS =========================================================
-def get_key(header, key) -> str:
-    with open('./properties.toml', 'r') as f:
-        data = toml.loads(f.read())
-        data = data[header][key]
-    return data
